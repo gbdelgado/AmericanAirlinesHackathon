e1ebf42be9f8ab567662d8aec5e5c5ad4110490d
Benni <gdelgado1@email.arizona.edu>
2020-01-18 10:00:10 -0700

    bootstrap added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const request = require('request');
 
 
-request('https://hackathon2020.herokuapp.com/flights?date=2020-01-01', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log(body);
+// request('https://hackathon2020.herokuapp.com/flights?date=2020-01-01', { json: true }, (err, res, body) => {
+//   if (err) { return console.log(err); }
+//   console.log(body);
+// });
+
+request('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=1f707fdd7f1d581cdae2d724a1bf0ffb', {json:true}, (err,res,body) =>{
+    if(err){return console.log(err);}
+    console.log(body);
 });
